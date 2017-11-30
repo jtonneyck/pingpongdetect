29cbcb38ef77ebe425ee0c0cd5d2f988fc21f906
Jurgen <j.tonneyck@gmail.com>
2017-12-01 00:36:57 +0100

    socket and button

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 let free = false
 app.get("/pingpong", (req, res) => {
-	console.log(req.query)
+	console.log("query", req.query.free)
 	if(req.query.free == "true") {
+		console.log("test true")
 		free = true
 	}
 	else if (req.query.free == "false") {
+		console.log("test false")
 		free = false
 	}
+	io.emit('pingStatus', free)
 	res.status(200).end()
 })
 
-app.get("/", (req, res) => {
-     if(free) res.status(200).send("The ping pong table is free!")
-     else if(!free) res.status(200).send("The ping pong table is occupied!")
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+// app.get("/", (req, res) => {
+//      if(free) res.status(200).send("The ping pong table is free!")
+//      else if(!free) res.status(200).send("The ping pong table is occupied!")
+// })
+
+app.get("/", (req, res)=> {
+	res.sendfile(__dirname + '/index.html')
 })
-app.listen(3000 , function() {
+server.listen(3000 , function() {
 	console.log('Node app is running on port', 3000);
 });
 
