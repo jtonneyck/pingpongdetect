4db94f95341ce3bb4242ac53fb687bf582d970e9
Jurgen <j.tonneyck@gmail.com>
2017-12-01 01:05:06 +0100

    static files

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+app.use(express.static('public'))
 app.set("port", process.env.port)
+
 let free = false
 app.get("/pingpong", (req, res) => {
 	console.log("query", req.query.free)
